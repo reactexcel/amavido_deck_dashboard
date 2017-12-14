ad763b72a1751eba31c58c102f5c1a48b6594f83
reactexcel <arun.etech2011@gmail.com>
2017-12-14 16:40:15 +0530

    second​ ​ time​ ​ the​ ​ state​ ​ has​ ​ to​ ​ be​ ​ read​ ​ from​ ​ localStorage

--- app/containers/HomePage/actions.js
+++ app/containers/HomePage/actions.js
@@ -8,43 +8,64 @@ import {
 
 export function getRegionsList() {
   return function(dispatch) {
-    return axios.get('https://staging-api.amavido.de/api/regions?fields=name%20headline')
-      .then((response) => {
-        return dispatch ( {
-          type: REGIONS_LIST,
-          regionsList: response.data,
-        });
-      })
-      .catch((err) => {
-        return dispatch ( {
-          type: REGIONS_LIST,
-          regionsList: []
-        });
-      })
+    let checkLocal = localStorage.getItem('regions_list');
+    if( checkLocal ){
+      return dispatch ( {
+        type: REGIONS_LIST,
+        regionsList: JSON.parse( checkLocal ).data
+      });
+    }else{
+      return axios.get('https://staging-api.amavido.de/api/regions?fields=name%20headline')
+        .then((response) => {
+          localStorage.setItem('regions_list', JSON.stringify(response));
+          return dispatch ( {
+            type: REGIONS_LIST,
+            regionsList: response.data,
+          });
+        })
+        .catch((err) => {
+          return dispatch ( {
+            type: REGIONS_LIST,
+            regionsList: []
+          });
+        })
+    }
   }
 }
 
 export function getRegionVillagesList(region) {
   return function(dispatch) {
-    return axios.get('https://staging-api.amavido.de/api/villages?fields=name%20headline&regionId='+region.id)
-      .then((response) => {
-        return dispatch ( {
-          type: VILLAGES_BOARD,
-          villagesBoard: {
-            region: region,
-            villages: response.data,
-          }
-        });
-      })
-      .catch((err) => {
-        return dispatch ( {
-          type: VILLAGES_BOARD,
-          villagesBoard: {
-            region: region,
-            villages: [],
-          }
-        });
-      })
+    let checkLocal = localStorage.getItem('villages_'+region.id);
+    if( checkLocal ){
+      return dispatch ( {
+        type: VILLAGES_BOARD,
+        villagesBoard: {
+          region: region,
+          villages: JSON.parse( checkLocal ).data,
+        }
+      });
+    }else{
+      return axios.get('https://staging-api.amavido.de/api/villages?fields=name%20headline&regionId='+region.id)
+        .then((response) => {
+          localStorage.setItem('villages_'+region.id, JSON.stringify(response));
+          return dispatch ( {
+            type: VILLAGES_BOARD,
+            villagesBoard: {
+              region: region,
+              villages: response.data,
+            }
+          });
+        })
+        .catch((err) => {
+          return dispatch ( {
+            type: VILLAGES_BOARD,
+            villagesBoard: {
+              region: region,
+              villages: [],
+            }
+          });
+        })
+    }
   }
 }
 
