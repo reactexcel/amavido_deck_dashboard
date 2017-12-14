df33a4649a159d851f8576fbc0dd0944db83e13e
reactexcel <arun.etech2011@gmail.com>
2017-12-14 15:28:19 +0530

    console error removed when same region is opened

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
+import _ from 'lodash'
 import messages from './messages';
 
 import * as actions from './actions';
@@ -32,13 +33,29 @@ export class HomePage extends Component { // eslint-disable-line react/prefer-st
     })
   }
 
+  _checkRegionLaneExist = (regionId) => {
+    let match = _.find(this.state.villagesBoard, function(o) { 
+      return o.region.id === regionId; 
+    });
+    if(match){
+      return true;
+    }
+    return false;
+  }
+
+  _doGetRegionVillagesList = (region) => {
+    if(!this._checkRegionLaneExist(region.id)){
+      this.props.getRegionVillagesList(region)
+    }
+  }
+
   render() {
     return (
     	<div>
         <BoardRegion 
           regionsList={this.state.regionsList}
           villagesBoard={this.state.villagesBoard}
-          getRegionVillagesList={this.props.getRegionVillagesList}
+          getRegionVillagesList={this._doGetRegionVillagesList}
           removeRegion={this.props.removeRegion}
         />
       </div>
